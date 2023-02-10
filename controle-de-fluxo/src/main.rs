5e26c29043288704e03012a07cc623352896b978
Marcelo Silva <marcelo@sonik.com.br>
2023-02-10 14:23:49 -0300

    controle de fluxo

---
+++ controle-de-fluxo/src/main.rs
@@ -0,0 +1,63 @@
+fn main() {
+    println!("Hello, world!");
+
+    //simple_loop();
+    loop_with_return();
+    loop_with_label();
+    for_methods();
+}
+
+fn simple_loop() {
+    loop {
+        println!("Execute again and again");
+    }
+}
+
+fn loop_with_return() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result of for scope is {result}");
+}
+
+fn loop_with_label() {
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+}
+
+fn for_methods() {
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {element}");
+    }
+
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
+}
